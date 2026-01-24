f9dfed91227ac0f5abb9a7771d079a83783ec503
nainasethia <nainasethia237@gmail.com>
2026-01-25 01:31:35 +0530

    Component- Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      <ListGroup
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
+      <Alert>Hello World</Alert>
     </div>
   );
 }
